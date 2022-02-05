43b964409268b0a6355e7289e51b49a08c412a43
Nilutpal Borgohain <borgohainnilutpal65@gmail.com>
2022-02-05 17:41:02 +0530

    updated problems

---
+++ CP/Codeforces/B_1467.cpp
@@ -0,0 +1,112 @@
+#ifdef ONLINE_JUDGE
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define POW2(X) (1<<(X))
+#define CKBIT(S,X) (((S)&POW2(X))!=0)
+const double pi=acos(-1.0);
+const double eps=1e-11;
+template<class T> inline void ckmin(T &a,T b){ a=min(a,b); }
+template<class T> inline void ckmax(T &a,T b){ a=max(a,b); }
+template<class T> inline T sqr(T x){ return x*x; }
+#define SIZE(A) ((int)A.size())
+#define LENGTH(A) ((int)A.length())
+#define MP(A,B) make_pair(A,B)
+#define PB(X) push_back(X)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,a) for(int i=0;i<(a);++i)
+#define ALL(A) A.begin(),A.end()
+#define ff first
+#define ss second
+using uint=unsigned int;
+using int64=long long int;
+using uint64=unsigned long long int;
+using ipair=pair<int,int>;
+using VI=vector<int>;
+using VD=vector<double>;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(int64 t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(uint64 t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+const int maxn = 3e5 + 5;
+int a[maxn];
+bool b[maxn];
+void solve() {
+	int n; cin >> n;
+	int ans = 0;
+	REP(i,n) {
+		cin >> a[i];
+		b[i] = 0;
+		if (i-1>0) {
+			if ((a[i-1]>a[i-2] && a[i-1]>a[i]) || (a[i-1]<a[i-2] && a[i-1]<a[i])) {
+				b[i-1] = 1;
+				ans++;
+			}
+			//cout << a[i-2] << " " << a[i-1] << " " << a[i] << endl;
+			//cout << ans << endl;
+		}
+		//debug(b[i]);
+	}
+	//debug(ans);
+	int ans1 = ans;
+	REP(i,n) {
+		if (b[i]) {
+			int res = 1;
+			if (b[i-1]) res++;
+			if (i+2<n && ((a[i+1]>a[i+2] && a[i+1]>a[i-1]) ||  (a[i+1]<a[i+2] && a[i+1]<a[i-1]))) {
+				if (!b[i+1]) res--;
+			} else if(b[i+1]) res++;
+			//debug(i);
+			//debug(res);
+			ans1 = min(ans1, ans - res);
+			res = 1;
+			if (b[i+1]) res++;
+			if (i-2>=0 && ((a[i-1]>a[i+1] && a[i-1]>a[i-2]) ||  (a[i-1]<a[i+1] && a[i-1]<a[i-2]))) {
+				if (!b[i-1]) res--;
+			} else if(b[i-1]) res++;
+			ans1 = min(ans1, ans - res);
+		}
+	}
+	cout << ans1 << "\n";
+}
+int main()
+{
+//#ifdef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+//#endif
+	std::ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int test; cin >> test;
+	while(test--) {
+		solve();
+	}
+
+	return 0;
+}
+
